3ef7535ce2ccc78483184d73e5d5d0a6f1b4b5e2
Hieu.cc <hieu.cc@extremevn.com.vn>
2021-12-06 12:10:45 +0700

    init pipeline db

--- database/demo_sql.sql
+++ database/demo_sql.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `tests` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `des` varchar(255) NOT NULL DEFAULT '',
-  `flg` BOOLEAN
+  `flg` BOOLEAN,
+  PRIMARY KEY (`id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='demo test';
