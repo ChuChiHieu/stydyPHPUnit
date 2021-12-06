ab1eff5cbd533204a8bbe2edb77374696f236fe6
Hieu.cc <hieu.cc@extremevn.com.vn>
2021-12-06 12:17:32 +0700

    init pipeline db

--- database/demo_sql.sql
+++ database/demo_sql.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `laravel` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `laravel`;
-CREATE TABLE IF NOT EXISTS `tests` (
+CREATE TABLE IF NOT EXISTS `tests_dump` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `des` varchar(255) NOT NULL DEFAULT '',
